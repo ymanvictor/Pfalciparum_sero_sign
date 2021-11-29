ac635a45b659e686d24006b962ea9ae1a9c3a1c2
Victor Yman <victor.yman@ki.se>
2021-11-29 19:26:10 +0100

    Update cloned_repo_init.R

--- src/cloned_repo_init.R
+++ src/cloned_repo_init.R
@@ -1,7 +1,8 @@
 ## project initiation script
-## when repo cloned from github with excisting 
+## when repo cloned from github with excisting 'renv.lock' file
+
 if (!require("renv")) install.packages("renv")
 
 library("renv")
-#renv::init()
+
 renv::activate()
